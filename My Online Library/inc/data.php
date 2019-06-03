d898c6e61d098b01dcc7b6bb908b2a67a9f328c0
priyakaajal <49344132+priyakaajal@users.noreply.github.com>
2019-06-03 18:27:39 -0400

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,178 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+  "title" => "Harry Potter and The Deathly Hallows",
+  "img" => "img/media/book1.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2011,
+ "category" => "Books",
+ "authors" => [
+     "J.K. Rowling",
+ ],
+ "publisher" => "Scholastic",
+ "isbn" => '9780545010221'
+];
+
+$catalog[102] = [
+  "title" => "Divergent",
+  "img" => "img/media/book2.jpg",
+ "genre" => "Fiction",
+ "format" => "Hardcover",
+ "year" => 2007,
+ "category" => "Books",
+ "authors" => [
+     "Veronica Roth",
+ ],
+ "publisher" => "HarperCollins",
+ "isbn" => '9788851127190'
+];
+
+$catalog[103] = [
+  "title" => "Harry Potter and The Prisoner of Azkaban",
+  "img" => "img/media/book3.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 1999,
+ "category" => "Books",
+ "authors" => [
+     "J.K. Rowling",
+ ],
+ "publisher" => "Scholastic",
+ "isbn" => '9781408855676'
+];
+
+$catalog[104] = [
+  "title" => "Milk and Honey",
+  "img" => "img/media/book4.jpg",
+ "genre" => "Poetry",
+ "format" => "Paperback",
+ "year" => 2014,
+ "category" => "Books",
+ "authors" => [
+     "Rupi Kaur",
+ ],
+ "publisher" => "Andrews McMeel Publishing",
+ "isbn" => '9781449474256'
+];
+
+//Movies
+$catalog[201] = [
+ "title" => "Spiderman Into The Spiderverse",
+ "img" => "img/media/movie1.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Peter Ramsey, Rodney Rothman, Bob Persichetti",
+ "writers" => [
+     "Rodney Rothman, Phil Lord"
+
+ ],
+ "stars" => [
+     "Jake Johnson",
+     "Shameik Moore",
+     "Hailee Steinfeld"
+ ],
+];
+
+$catalog[202] = [
+ "title" => "Black Panther",
+ "img" => "img/media/movie2.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Ryan Coogler",
+ "writers" => [
+     "Ryan Coogler, Joe Robert Cole"
+
+ ],
+ "stars" => [
+     "Chadwick Boseman",
+     "Lupita Nyong",
+     "Michael B Jordan"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Incredibles 2",
+ "img" => "img/media/movie3.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Brad Bird",
+ "writers" => [
+     "Brad Bird"
+
+ ],
+ "stars" => [
+     "Brad Bird",
+     "Holly Hunter",
+     "Craig T. Nelson",
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Fast and Furious 6",
+ "img" => "img/media/movie4.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 2013,
+ "category" => "Movies",
+ "director" => "Justin Lin",
+ "writers" => [
+     "Chris Morgan"
+
+ ],
+ "stars" => [
+     "Paul Walker",
+     "Vin Diesel",
+     "Dwayne Johnson",
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "Malibu Nights",
+ "img" => "img/media/album1.jpg",
+ "genre" => "Pop, indie",
+ "format" => "CD",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "LANY"
+];
+
+$catalog[302] = [
+ "title" => "American Teen",
+ "img" => "img/media/album2.jpg",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 2017,
+ "category" => "Music",
+ "artist" => "Khalid"
+];
+
+$catalog[303] = [
+ "title" => "Kinda",
+ "img" => "img/media/album3.jpg",
+ "genre" => "Pop, indie",
+ "format" => "CD",
+ "year" => 2016,
+ "category" => "Music",
+ "artist" => "LANY"
+];
+
+$catalog[304] = [
+ "title" => "Persona",
+ "img" => "img/media/album4.jpg",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 2019,
+ "category" => "Music",
+ "artist" => "BTS"
+];
+
+?> 
